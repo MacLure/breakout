6647dc9881603833cba6ff8791e2eca2fad9a070
Malcolm <malcolm.maclure@gmail.com>
2019-03-18 15:37:19 -0400

    initial commit

---
+++ src/dependencies.lua
@@ -0,0 +1,10 @@
+push = require 'lib/push'
+Class = require 'lib/class'
+
+require 'src/constants'
+require 'src/StateMachine'
+require 'src/util'
+
+require 'src/states/BaseState'
+require 'src/states/PlayState'
+require 'src/states/StartState'
